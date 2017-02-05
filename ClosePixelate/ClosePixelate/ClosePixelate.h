b57f92615d0fc4e1cc9cb0a3211bbb32dadacc1c
bmaslakov <b.maslakov@gmail.com>
2017-02-05 23:09:48 +0300

    initial commit

---
+++ ClosePixelate/ClosePixelate/ClosePixelate.h
@@ -0,0 +1,19 @@
+//
+//  ClosePixelate.h
+//  ClosePixelate
+//
+//  Created by Boris on 05/02/2017.
+//  Copyright © 2017 bmaslakov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ClosePixelate.
+FOUNDATION_EXPORT double ClosePixelateVersionNumber;
+
+//! Project version string for ClosePixelate.
+FOUNDATION_EXPORT const unsigned char ClosePixelateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ClosePixelate/PublicHeader.h>
+
+
