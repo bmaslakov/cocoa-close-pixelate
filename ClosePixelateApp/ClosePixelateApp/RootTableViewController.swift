ed81c10c3302cfbb83839809bdcd6f8e25d86382
bmaslakov <b.maslakov@gmail.com>
2017-02-09 22:13:42 +0300

    add extra test images

--- ClosePixelateApp/ClosePixelateApp/RootTableViewController.swift
+++ ClosePixelateApp/ClosePixelateApp/RootTableViewController.swift
@@ -6,46 +6,186 @@ public class RootTableViewController: UITableViewController {
     private let cellId = "Cell"
     private let segueId = "Segue1"
     
-    private let imageResources = [ "officer", "stanley" ]
-    private let layers = [
-        [
-            PixelateLayer(.diamond) {
-                $0.resolution = 48
-                $0.size = 50
-            },
-            PixelateLayer(.diamond) {
-                $0.resolution = 48
-                $0.offset = 24
-            },
-            PixelateLayer(.diamond) {
-                $0.resolution = 8
-                $0.size = 6
-            }
-        ],
-        [
-            PixelateLayer(.square) {
-                $0.resolution = 32
-            },
-            PixelateLayer(.circle) {
-                $0.resolution = 32
-                $0.offset = 15
-            },
-            PixelateLayer(.circle) {
-                $0.resolution = 32
-                $0.size = 26
-                $0.offset = 13
-            },
-            PixelateLayer(.circle) {
-                $0.resolution = 32
-                $0.size = 18
-                $0.offset = 10
-            },
-            PixelateLayer(.circle) {
-                $0.resolution = 32
-                $0.size = 12
-                $0.offset = 8
-            }
-        ]
+    private let configurations = [
+        (
+            "officer",
+            [
+                PixelateLayer(.diamond) {
+                    $0.resolution = 48
+                    $0.size = 50
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 48
+                    $0.offset = 24
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 8
+                    $0.size = 6
+                }
+            ]
+        ), (
+            "stanley",
+            [
+                PixelateLayer(.square) {
+                    $0.resolution = 32
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.offset = 15
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.size = 26
+                    $0.offset = 13
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.size = 18
+                    $0.offset = 10
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.size = 12
+                    $0.offset = 8
+                }
+            ]
+        ), (
+            "take-my-portrait",
+            [
+                PixelateLayer(.square) {
+                    $0.resolution = 48
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 48
+                    $0.offset = 12
+                    $0.alpha = 0.5
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 48
+                    $0.offset = 36
+                    $0.alpha = 0.5
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 16
+                    $0.size = 8
+                    $0.offset = 4
+                }
+            ]
+        ), (
+            "tony",
+            [
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.size = 6
+                    $0.offset = 8
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.size = 9
+                    $0.offset = 8
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.size = 12
+                    $0.offset = 24
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.size = 9
+                    $0.offset = 0
+                }
+            ]
+        ), (
+            "wonder",
+            [
+                PixelateLayer(.diamond) {
+                    $0.resolution = 24
+                    $0.size = 25
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 24
+                    $0.offset = 12
+                },
+                PixelateLayer(.square) {
+                    $0.resolution = 24
+                    $0.alpha = 0.6
+                }
+            ]
+        ), (
+            "anita",
+            [
+                PixelateLayer(.square) {
+                    $0.resolution = 32
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.offset = 16
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 32
+                    $0.offset = 0
+                    $0.alpha = 0.5
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 16
+                    $0.size = 9
+                    $0.offset = 0
+                    $0.alpha = 0.5
+                }
+            ]
+        ), (
+            "giraffe",
+            [
+                PixelateLayer(.circle) {
+                    $0.resolution = 24
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 24
+                    $0.size = 9
+                    $0.offset = 12
+                }
+            ]
+        ), (
+            "kendra",
+            [
+                PixelateLayer(.square) {
+                    $0.resolution = 48
+                    $0.offset = 12
+                },
+                PixelateLayer(.circle) {
+                    $0.resolution = 48
+                    $0.offset = 0
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 16
+                    $0.size = 15
+                    $0.offset = 0
+                    $0.alpha = 0.6
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 16
+                    $0.size = 15
+                    $0.offset = 8
+                    $0.alpha = 0.6
+                }
+            ]
+        ), (
+            "gavin",
+            [
+                PixelateLayer(.square) {
+                    $0.resolution = 48
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 12
+                    $0.size = 8
+                },
+                PixelateLayer(.diamond) {
+                    $0.resolution = 12
+                    $0.size = 8
+                    $0.offset = 6
+                }
+            ]
+        )
     ]
     
     public override func viewDidLoad() {
@@ -57,12 +197,12 @@ public class RootTableViewController: UITableViewController {
     }
     
     public override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return imageResources.count
+        return configurations.count
     }
     
     public override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        cell.textLabel?.text = imageResources[indexPath.row]
+        cell.textLabel?.text = configurations[indexPath.row].0
         return cell
     }
     
@@ -71,8 +211,7 @@ public class RootTableViewController: UITableViewController {
             let indexPath = self.tableView.indexPathForSelectedRow
             let destination = segue.destination as! ViewController
             let index = (indexPath?.row)!
-            destination.imageId = imageResources[index]
-            destination.layers = layers[index]
+            destination.config = configurations[index]
         }
     }
 }
